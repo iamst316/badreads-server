757468edbde2b6d366b1a3d0e43a1cc3c96730bb
Shubham Tandon <iamshubham316@gmail.com>
2023-11-29 09:07:03 +0530

    first commit

---
+++ src/main/java/com/badreads/badreads/BadreadsApplication.java
@@ -0,0 +1,13 @@
+package com.badreads.badreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadreadsApplication.class, args);
+	}
+
+}
